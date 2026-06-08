117e2c577ea524f46d8749daeefac56aef315c44
Algebrazebra <Algebrazebra@users.noreply.github.com>
2026-06-08 18:48:34 +0300

    Add MonthDay entity

---
+++ src/test/scala/cronh/domain/HourTest.scala
@@ -0,0 +1,13 @@
+package cronh.domain
+
+import munit.ScalaCheckSuite
+import org.scalacheck.Prop.*
+
+class HourTest extends ScalaCheckSuite {
+
+  property("Only valid hour values can be created") = forAll { (n: Int) =>
+    if (n < 0 || n >= 24) intercept[IllegalArgumentException](Hour(n)): Unit
+    else Hour(n): Unit
+  }
+
+}
