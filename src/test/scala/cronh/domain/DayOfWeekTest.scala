5d0721feaf1668c31821a721787334dc5700cf79
Algebrazebra <Algebrazebra@users.noreply.github.com>
2026-06-09 19:50:19 +0300

    Add ordering law properties to Month and DayOfWeek tests

--- src/test/scala/cronh/domain/DayOfWeekTest.scala
+++ src/test/scala/cronh/domain/DayOfWeekTest.scala
@@ -1,8 +1,12 @@
 package cronh.domain
 
-import munit.FunSuite
+import cronh.domain.Generators.given
+import munit.ScalaCheckSuite
+import org.scalacheck.Prop.*
 
-class DayOfWeekTest extends FunSuite {
+class DayOfWeekTest extends ScalaCheckSuite {
+
+  private val ord = summon[Ordering[DayOfWeek]]
 
   test("DayOfWeek enumerates all seven days") {
     assertEquals(DayOfWeek.values.length, 7)
@@ -14,9 +18,27 @@ class DayOfWeekTest extends FunSuite {
     assertEquals(sorted.last, DayOfWeek.Sunday)
   }
 
-  test("DayOfWeek ordering is consistent with declaration order") {
-    val ord = summon[Ordering[DayOfWeek]]
-    assert(ord.lt(DayOfWeek.Monday, DayOfWeek.Friday))
-    assert(ord.gt(DayOfWeek.Sunday, DayOfWeek.Saturday))
+  property("DayOfWeek ordering is reflexive") = forAll { (d: DayOfWeek) =>
+    ord.compare(d, d) == 0
+  }
+
+  property("DayOfWeek ordering is antisymmetric") = forAll {
+    (a: DayOfWeek, b: DayOfWeek) =>
+      ord.compare(a, b).sign == -ord.compare(b, a).sign
+  }
+
+  property("DayOfWeek ordering is transitive") = forAll {
+    (a: DayOfWeek, b: DayOfWeek, c: DayOfWeek) =>
+      if (ord.lteq(a, b) && ord.lteq(b, c)) ord.lteq(a, c)
+      else true
+  }
+
+  property("DayOfWeek ordering agrees with .ordinal") = forAll {
+    (a: DayOfWeek, b: DayOfWeek) =>
+      ord.compare(a, b).sign == a.ordinal.compare(b.ordinal).sign
+  }
+
+  property("DayOfWeek equality is reflexive") = forAll { (d: DayOfWeek) =>
+    d == d
   }
 }
