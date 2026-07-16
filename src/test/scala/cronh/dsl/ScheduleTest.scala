9a3bafb50d786418bbda7be01dd885868586a0f1
Algebrazebra <Algebrazebra@users.noreply.github.com>
2026-07-17 01:00:46 +0200

    WIP

--- src/test/scala/cronh/dsl/ScheduleTest.scala
+++ src/test/scala/cronh/dsl/ScheduleTest.scala
@@ -1,14 +1,92 @@
 package cronh.dsl
 
+import cronh.domain.Generators.{
+  given_Arbitrary_DayOfMonth,
+  given_Arbitrary_DayOfWeek
+}
+import cronh.domain.fieldTypes.{DayOfMonth, DayOfWeek}
+import cronh.domain.fieldTypes.Month.{February, January, March}
 import cronh.render.toCron
-import munit.FunSuite
+import munit.{FunSuite, ScalaCheckSuite}
+import org.scalacheck.Prop.forAll
 
-class ScheduleTest extends FunSuite {
+class ScheduleTest extends ScalaCheckSuite {
 
-  // Test month selection works: single month, many months
+  // The tests should test created cron schedules against the string
+  // The following day cases should be tested with (everyminute); no time phase
+  // 1. [x] One month, multiple months, multiple month days
+  // 2. [x] One weekday, two weekdays, three weekdays
+  // 3. [x] One month day, two month days, three month days
+  // 4. [x]  Weekdays, Weekends shortcut is available (both in DSL and both as alias)
+  // 5  [x] Test that makes sure we can select Weekday and then it's possible to select MonthDay
+  //        Test that makes sure we can select Month Day and then it's possible to select Weekday
+  // 6. ranges: for month, weekdays and month days incl. wraparound
+  //    check they are inclusive and covering
+  // TODO: test wrap around ranges. For months: Now to March --> 11-12, 1-3
+  test("DSL allows selection of one month or multiple months") {
+    assertEquals(Schedule.in(January).daily.everyMinute.toCron, "* * * 1 *")
+    assertEquals(
+      Schedule.in(January, February).daily.everyMinute.toCron,
+      "* * * 1,2 *"
+    )
+    assertEquals(
+      Schedule.in(January, February, March).daily.everyMinute.toCron,
+      "* * * 1,2,3 *"
+    )
+  }
 
-  // Test month range: inclusive and convering. is field.range
-  // --> Test wraparound ranges
+  test("DSL has a weekdays shortcut") {
+    assertEquals(
+      Schedule.weekdays.everyMinute.toCron,
+      Schedule.on(Weekdays).everyMinute.toCron
+    )
+  }
+
+  test("DSL has a weekend shortcut") {
+    assertEquals(
+      Schedule.weekends.everyMinute.toCron,
+      Schedule.on(Weekends).everyMinute.toCron
+    )
+  }
+
+  test("DSL allows selection of one weekday or multiple weekdays") {
+    assertEquals(Schedule.on(Mondays).everyMinute.toCron, "* * * * 1")
+    assertEquals(
+      Schedule.on(Mondays, Tuesdays).everyMinute.toCron,
+      "* * * * 1,2"
+    )
+    assertEquals(
+      Schedule.on(Mondays, Tuesdays, Wednesdays).everyMinute.toCron,
+      "* * * * 1,2,3"
+    )
+  }
+
+  test("DSL allows selection of one day of month or multiple days of month") {
+    assertEquals(Schedule.onThe(1.st).everyMinute.toCron, "* * 1 * *")
+    assertEquals(Schedule.onThe(1.st, 2.nd).everyMinute.toCron, "* * 1,2 * *")
+    assertEquals(
+      Schedule.onThe(1.st, 2.nd, 3.rd).everyMinute.toCron,
+      "* * 1,2,3 * *"
+    )
+  }
+
+  property("Weekday can be set after the day of month was set and vice versa") =
+    forAll { (dom: DayOfMonth, weekday: DayOfWeek) =>
+      assertEquals(
+        Schedule.onThe(dom).orOn(weekday).everyMinute.toCron,
+        Schedule.on(weekday).orOnThe(dom).everyMinute.toCron
+      )
+    }
+
+  // Test both long and short form of Months, Weekdays are possible.
+
+  // The tests should test created cron schedules against the string
+  // The following time cases should be tested with holding the day cases constant: daily
+  // 1. Setting specific time with time literal and 24h format
+  // 2. Setting specific time with time literal and 12h format
+  // 3. Setting time with .h and .min
+  // 4. Setting time with multiple .h and multiple .min
+  // 5. Every minute is available (both immediately and after setting hours)
 
   // Test Day ranges: one, many, day range -->  what happens with a wraparound?
   // TODO: Document: from 15th june to 15th of august is NOT possible in cron
@@ -51,24 +129,4 @@ class ScheduleTest extends FunSuite {
     assertEquals(Schedule.onThe(1.st, 15.th).at(6.h).toCron, "0 6 1,15 * *")
   }
 
-  /*
-  test(".on(Weekdays) accepts a prebuilt field") {
-    assertEquals(Schedule.on(Weekdays).at(9.h).toCron, "0 9 * * 1-5")
-  }
-  
-  test("Mon to Fri builds an inclusive weekday range") {
-    assertEquals(Schedule.on(Monday to Friday).at(9.h).toCron, "0 9 * * 1-5")
-  }
-
-  test("Tue to Thu renders the middle of the week") {
-    assertEquals(Schedule.on(Tue to Thu).at(9.h).toCron, "0 9 * * 2-4")
-  }
-
-  test("Monday to Friday: long-form day aliases work in a range") {
-    assertEquals(
-      Schedule.on(Monday to Friday).at(9.h).toCron,
-      "0 9 * * 1-5"
-    )
-  }
-   */
 }
